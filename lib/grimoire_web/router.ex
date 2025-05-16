90ce8a3cafab25e95a8e6e83bd752a25a1677726
Brent Yoder <499565+InAbsentia@users.noreply.github.com>
2025-05-16 17:12:31 -0600

    Generate user authentication

--- lib/grimoire_web/router.ex
+++ lib/grimoire_web/router.ex
@@ -1,6 +1,8 @@
 defmodule GrimoireWeb.Router do
   use GrimoireWeb, :router
 
+  import GrimoireWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule GrimoireWeb.Router do
     plug :put_root_layout, html: {GrimoireWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule GrimoireWeb.Router do
       live_dashboard "/dashboard", metrics: GrimoireWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", GrimoireWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{GrimoireWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", GrimoireWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{GrimoireWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
